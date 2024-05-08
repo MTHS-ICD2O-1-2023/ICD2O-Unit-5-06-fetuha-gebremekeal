8c79342ca23a369b0170ecb428a5a84273f0abff
Fethawit <159185142+fetuha-gebremekeal@users.noreply.github.com>
2024-05-08 17:54:01 +0000

    js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,21 @@
-// Copyright (c) 2020 Mr. Coxall All rights reserved
+// Copyright (c) 2024 Fetuha All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by:Fetuha
+// Created on: May 2024
 // This file contains the JS functions for index.html
+"use strict"
+
+function additionClicked() {
+  const num1 = parseInt(document.getElementById("num1-entered").value)
+  const num2 = parseInt(document.getElementById("num2-entered").value)
+  let counter = 0
+  let answer = 0
+
+  while (counter < num2) {
+    answer += Math.abs(num1)
+    counter++
+  }
+  console.log(answer)
+  document.getElementById("answer").innerHTML = "The answer is = " + answer
+}
+
